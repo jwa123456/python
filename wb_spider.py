3629506a2458ca549fe246198bab05ab8c78cf46
wei.jiang <wei.jiang@17zuoye.com>
2017-01-22 22:14:17 +0800

    fix timer and content

--- wb_spider.py
+++ wb_spider.py
@@ -22,13 +22,13 @@ else:
     user_id = (int)(raw_input(u"请输入user_id: "))
 
 cookie = {
-    "Cookie": "_T_WM=f5d5a293c5de4d66c7d3c52fd5bc177e; ALF=1487410225; SCF=AgKYWDQjkoRzF68ansAXGnup7vCW0nq8KFLM9YQ1Hq_vqxzFg86RF5VuhXXtMi3CP6GE-vGluQ24qOIIP0dssJw.; SUB=_2A251hPdtDeTxGedG71YS8S_LzjuIHXVWhpklrDV6PUJbktBeLWfckW1bLa4HG5raT71I2KplG3kUqarkJQ..; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WhaEwne.zfMm2zPgxAZiZxN5JpX5o2p5NHD95Qp1hBXe02pS0-NWs4DqcjeBPS0qPS0IgRReo5N; SUHB=0U1YF2ohMBukYR"}
-url = 'http://weibo.cn/u/%d?filter=1&page=1' % user_id
+    "Cookie": "SCF=Av3mJnE4NBa5e6XgVKv4VZwcgSEuInee_OOmscxrLGwWqQzTYTHwpnWc9B_tlFYbf8_Cc4OOrPWgLOrDFgh0CDU.; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WhaEwne.zfMm2zPgxAZiZxN5JpX5o2p5NHD95Qp1hBXe02pS0-NWs4DqcjeBPS0qPS0IgRReo5N; SUHB=0HTwqvfFGbqUqN; _T_WM=2e46d2667a96be07d8d818bcf54c0267; SUB=_2A251ewy_DeTxGedG71YS8S_LzjuIHXVWh5T3rDV6PUJbkdAKLROhkW1Ef8nndpTwYbI0XuN2WIyx1OfMEg..; gsid_CTandWM=4uE55d871nBQuvUmJi4Oe7JMXcd"}
+url = 'http://weibo.cn/u/%d' % user_id
 
 html = requests.get(url, cookies=cookie).content
 selector = etree.HTML(html)
-pageNum = (int)(selector.xpath('//input[@name="mp"]')[0].attrib['value'])
-
+# pageNum = (int)(selector.xpath('//input[@name="mp"]')[0].attrib['value'])
+pageNum = 1274
 result = ""
 urllist_set = set()
 word_count = 1
@@ -39,21 +39,39 @@ print u'爬虫准备就绪...'
 for page in range(1, pageNum + 1):
 
     # 获取lxml页面
-    url = 'http://weibo.cn/u/%d?filter=1&page=%d' % (user_id, page)
+    url = 'http://weibo.cn/u/%d?page=%d' % (user_id, page)
     lxml = requests.get(url, cookies=cookie).content
     # 文字爬取
     selector = etree.HTML(lxml)
     content = selector.xpath('//span[@class="ctt"]')
+    timer = selector.xpath('//span[@class="ct"]')
+    print timer
     for each in content:
         text = each.xpath('string(.)')
+        index = content.index(each)
         if word_count >= 4:
-            text = "%d :" % (word_count - 3) + text + "\n\n"
+            if each.xpath('string(.//a)') == '全文':
+                # print each.xpath('string(.//a/@href)')
+                content_realUrl = each.xpath('string(.//a/@href)')
+                content_url = 'http://weibo.cn/%s' % content_realUrl
+                # print content_url
+                content_lxml = requests.get(content_url, cookies=cookie).content
+                # print content_lxml
+                content_selector = etree.HTML(content_lxml)
+                real_content = content_selector.xpath('//span[@class="ctt"]')
+                if len(real_content):
+                    text = real_content[0].xpath('string(.)')
+            else:
+                text = each.xpath('string(.)')
+            timer_text = timer[index - 3].xpath('string(.)')
+            text = "%d :" % (word_count - 3) + text + timer_text + "\n\n"
         else:
             text += "\n\n"
         result = result + text
         word_count += 1
-    time.sleep(10)
-fo = open("D:/python/project/%s" % user_id, "wb")
+    print "finish %d" % page
+    time.sleep(2)
+fo = open("/opt/weibo/%s" % user_id, "wb")
 fo.write(result)
 word_path = os.getcwd() + '/%d' % user_id
 print u'文字微博爬完毕'
